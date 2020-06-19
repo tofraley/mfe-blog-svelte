77a8a767093cb97e82eb31a71339b37660d92057
Taylor Fraley <tfraley@clearent.com>
2020-06-18 20:00:16 -0400

    pull app urls to import map and change page title

--- shell/src/-root-config.js
+++ shell/src/-root-config.js
@@ -2,13 +2,13 @@ import { registerApplication, start } from "single-spa";
 
 registerApplication({
   name: "nav-bar",
-  app: () => System.import("http://localhost:55453/-nav-bar.js"),
+  app: () => System.import("nav-bar"),
   activeWhen: ["/"]
 });
 
 registerApplication({
-  name: "blog-post-list",
-  app: () => System.import("http://localhost:5000/-blog-post-list.js"),
+  name: "home",
+  app: () => System.import("blog-post-list"),
   activeWhen: ["/home"]
 });
 
